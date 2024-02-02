732cda1a439a0b458dc8f3d6873d325e9c173b34
RenanCalvis <renancalvis@gmail.com>
2024-02-01 21:25:51 -0300

    Implementada funcionalidade nos botões de perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function menuAtivo(link){
 }
 links.forEach(menuAtivo);
 
-//-Ativação de items por parametro
+//-Ativação de items por parametro via URl
 const parametros = new URLSearchParams(location.search);
 
 // Utilizando arrow Function.
@@ -20,4 +20,19 @@ parametros.forEach((parametro) => {
         elemento.checked = true;
     }
 });
+
+//Perguntas Frequentes button
+const perguntas = document.querySelectorAll(".perguntas button");
+
+perguntas.forEach( eventosPerguntas = (pergunta) =>{
+    pergunta.addEventListener("click", ativarPergunta = (event) =>{
+        const pergunta = event.currentTarget;
+        const controls = pergunta.getAttribute("aria-controls");
+        const resposta = document.getElementById(controls);
+
+        resposta.classList.toggle('ativo');
+        const ativo = resposta.classList.contains("ativo"); //Retorna um boolean que é passado na função abaixo
+        pergunta.setAttribute("aria-expanded", ativo);
+    })
+})
 
